7ef137d459c24cb4dab58b8dd0578fb721e65ee0
Mate Papp <mate.papp@supercharge.io>
2019-04-08 13:18:31 +0000

    Initial blockly-ios integration via CocoaPods

---
+++ RevolutionRobotics/Extensions/UIView+Reusable.swift
@@ -0,0 +1,15 @@
+//
+//  UIView+Reusable.swift
+//  RevolutionRobotics
+//
+//  Created by Mate Papp on 2019. 04. 01..
+//  Copyright © 2019. Revolution Robotics. All rights reserved.
+//
+
+import UIKit
+
+extension Reusable where Self: UIView {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
