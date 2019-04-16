86c73ac7c7cb36eab279674f7094412c2265e9ed
Gabor Nagy Farkas <gabor.nagy.farkas@supercharge.io>
2019-04-16 18:02:51 +0200

    Add button highlights

---
+++ RevolutionRobotics/Utils/Extensions/UIImage+Color.swift
@@ -0,0 +1,25 @@
+//
+//  UIImage+Color.swift
+//  RevolutionRobotics
+//
+//  Created by Gabor Nagy Farkas on 2019. 04. 16..
+//  Copyright © 2019. Revolution Robotics. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    static func from(color: UIColor) -> UIImage? {
+        let rect = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
+        UIGraphicsBeginImageContext(rect.size)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+
+        context.setFillColor(color.cgColor)
+        context.fill(rect)
+
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return image
+    }
+}
