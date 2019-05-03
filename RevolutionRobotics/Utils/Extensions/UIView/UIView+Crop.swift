5da6349213000d59ed1bde2c95b2d94554d7086e
Robert Klacso <robert.klacso@supercharge.io>
2019-05-03 11:36:18 +0200

    [RRI-74] Add custom inputfield

--- RevolutionRobotics/Utils/Extensions/UIView/UIView+Crop.swift
+++ RevolutionRobotics/Utils/Extensions/UIView/UIView+Crop.swift
@@ -86,6 +86,13 @@ extension UIView {
 
     private func createPath(with radius: CGFloat, showTopArrow: Bool, croppedCorners: [Corner]) -> UIBezierPath {
         let path = UIBezierPath()
+        var labelFrame: CGRect?
+
+        if let superView = self.superview {
+            for view in superView.subviews where view.tag == 1 {
+                labelFrame = view.frame
+            }
+        }
 
         path.move(to: CGPoint(x: croppedCorners.contains(.topLeft) ? radius : 0.0, y: 0.0))
         path.addLine(to: CGPoint(x: 0.0, y: radius))
@@ -105,6 +112,11 @@ extension UIView {
             path.addLine(to: CGPoint(x: frame.size.width / 2, y: -radius))
             path.addLine(to: CGPoint(x: (frame.size.width / 2) - radius, y: 0.0))
         }
+        if let frame = labelFrame {
+            path.addLine(to: CGPoint(x: frame.maxX + 5, y: 0.0))
+            path.move(to: CGPoint(x: frame.minX - 5, y: 0.0))
+        }
+        path.addLine(to: CGPoint(x: croppedCorners.contains(.topLeft) ? radius : 0.0, y: 0.0))
         path.close()
 
         return path
---
+++ RevolutionRobotics/View/InputField/RRInputField.swift
@@ -0,0 +1,76 @@
+//
+//  RRInputField.swift
+//  RevolutionRobotics
+//
+//  Created by Robert Klacso on 2019. 05. 02..
+//  Copyright © 2019. Revolution Robotics. All rights reserved.
+//
+
+import UIKit
+
+final class RRInputField: RRCustomView {
+    // MARK: - Constants
+    enum Constants {
+        static let returnText = "\n"
+    }
+
+    // MARK: - Outlets
+    @IBOutlet private weak var borderView: UIView!
+    @IBOutlet private weak var textView: UITextView!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+
+    // MARK: - Variables
+    private var placeholder: String?
+    private var characterLimit: Int?
+}
+
+// MARK: - View lifecycle
+extension RRInputField {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        borderView.setBorder(fillColor: .clear, strokeColor: Color.brownGrey)
+        textView.delegate = self
+        textView.textContainer.maximumNumberOfLines = 2
+        textView.tintAdjustmentMode = .normal
+        textView.tintColor = .white
+    }
+}
+
+// MARK: - Setup
+extension RRInputField {
+    func setup(with description: String, placeholder: String, characterLimit: Int) {
+        descriptionLabel.text = description
+        textView.text = placeholder
+        self.placeholder = placeholder
+        self.characterLimit = characterLimit
+        self.layoutIfNeeded()
+    }
+}
+
+// MARK: - UITextViewDelegate
+extension RRInputField: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if let placeholder = self.placeholder, textView.text == placeholder, textView.textColor == Color.brownGrey {
+            textView.text = nil
+            textView.textColor = .white
+        }
+    }
+
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = placeholder
+            textView.textColor = Color.brownGrey
+        }
+    }
+
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == Constants.returnText {
+            textView.resignFirstResponder()
+        }
+        if let limit = characterLimit, range.location >= limit || text.count >= limit {
+            return false
+        }
+        return true
+    }
+}
