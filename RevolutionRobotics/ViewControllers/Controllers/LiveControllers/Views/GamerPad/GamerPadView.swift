30349326fc84c7ac615356fd4d94048870050d64
Pável Áron <aron@revolutionrobotics.org>
2019-10-16 15:45:24 +0200

    MOBILEAPP-226: Removed ready button from controller screen in onboarding

--- RevolutionRobotics/ViewControllers/Controllers/LiveControllers/Views/GamerPad/GamerPadView.swift
+++ RevolutionRobotics/ViewControllers/Controllers/LiveControllers/Views/GamerPad/GamerPadView.swift
@@ -19,12 +19,6 @@ final class GamerPadView: UIView, PlayablePadView {
     var xAxisPositionChanged: CallbackType<CGFloat>?
     var yAxisPositionChanged: CallbackType<CGFloat>?
     var buttonTapped: CallbackType<PressedPadButton>?
-    var onboardingReadyCallback: Callback? {
-        didSet {
-            guard onboardingReadyCallback != nil else { return }
-            setupOnboardingReadyButton()
-        }
-    }
 
     func configure(programs: [ProgramDataModel?]) {
         programs.enumerated().forEach { [weak self] in
@@ -35,29 +29,10 @@ final class GamerPadView: UIView, PlayablePadView {
     }
 }
 
-// MARK: - Private methoods
-extension GamerPadView {
-    private func setupOnboardingReadyButton() {
-        guard onboardingReadyCallback != nil else { return }
-        readyButton.setBorder(fillColor: .clear, strokeColor: .white, croppedCorners: [.bottomLeft, .topRight])
-
-        readyButton.setTitle(ControllerKeys.ready.translate(), for: .normal)
-        readyButton.isHidden = false
-    }
-}
-
 // MARK: - View lifecycle
 extension GamerPadView {
     override func awakeFromNib() {
         super.awakeFromNib()
         setupJoystickScene(in: joystickContainer)
-        setupOnboardingReadyButton()
-    }
-}
-
-// MARK: - Actions
-extension GamerPadView {
-    @IBAction private func readyButtonTapped(_ sender: Any) {
-        onboardingReadyCallback?()
     }
 }
