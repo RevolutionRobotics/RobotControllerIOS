62d1a8816c06524ba85e7a31d9b203486eac3805
Áron Pável <aron@revolutionrobotics.org>
2020-02-21 11:26:45 +0100

    MOBLEAPP-425: Fixed crash on accidentally double tapping challenges card

--- RevoRobotics/ViewControllers/NavigationController/RRNavigationController.swift
+++ RevoRobotics/ViewControllers/NavigationController/RRNavigationController.swift
@@ -9,11 +9,44 @@
 import UIKit
 
 final class RRNavigationController: UINavigationController {
+    private var pushedType: String?
+
     override var shouldAutorotate: Bool {
         return true
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return .landscapeRight
+    }
+
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        let targetType = String(describing: type(of: viewController))
+        guard targetType != pushedType else {
+            return
+        }
+
+        pushedType = String(describing: type(of: viewController))
+        super.pushViewController(viewController, animated: animated)
+    }
+
+    override func popViewController(animated: Bool) -> UIViewController? {
+        let target = super.popViewController(animated: animated)
+        pushedType = String(describing: type(of: target))
+
+        return target
+    }
+
+    override func popToRootViewController(animated: Bool) -> [UIViewController]? {
+        let target = viewControllers.first
+        pushedType = target != nil
+            ? String(describing: type(of: target))
+            : nil
+
+        return super.popToRootViewController(animated: animated)
+    }
+
+    override func popToViewController(_ viewController: UIViewController, animated: Bool) -> [UIViewController]? {
+        pushedType = String(describing: type(of: viewController))
+        return super.popToViewController(viewController, animated: animated)
     }
 }
