4a23f4018df9293dcf17401e004f898ca09031b3
Pável Áron <aron@revolutionrobotics.org>
2021-02-18 12:31:44 +0100

    Fixed default slider positions for dirver pad controller

---
+++ RevoRobotics/Utils/Extensions/UIImage/UIImage+Rotation.swift
@@ -0,0 +1,39 @@
+//
+//  UIImage.swift
+//  RevoRobotics
+//
+//  Created by Pável Áron on 2021. 02. 18..
+//  Copyright © 2021. Revolution Robotics. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    func rotate(radians: Float) -> UIImage? {
+        var newSize = CGRect(origin: .zero, size: self.size)
+            .applying(CGAffineTransform(rotationAngle: CGFloat(radians))).size
+
+        // Trim off the extremely small float value to prevent core graphics from rounding it up
+        newSize.width = floor(newSize.width)
+        newSize.height = floor(newSize.height)
+
+        UIGraphicsBeginImageContextWithOptions(newSize, false, self.scale)
+        let context = UIGraphicsGetCurrentContext()!
+
+        // Move origin to middle
+        context.translateBy(x: newSize.width / 2, y: newSize.height / 2)
+        // Rotate around middle
+        context.rotate(by: CGFloat(radians))
+        // Draw the image at its center
+        self.draw(in: CGRect(
+            x: -self.size.width / 2,
+            y: -self.size.height / 2,
+            width: self.size.width,
+            height: self.size.height))
+
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return newImage
+    }
+}
